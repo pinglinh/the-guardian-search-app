e74c04c758b3e7ccac9f6e2df7b9e4d52be161c3
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-04-01 13:42:35 +0100

    Remove return statements in SearchResults component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,19 +1,15 @@
 import React from "react";
 
-const SearchResults = props => {
-  return (
-    <ul>
-      {props.articles.map((article, index) => {
-        return (
-          <li key={index}>
-            <a href={article.webUrl} target="_blank">
-              {article.webTitle}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+const SearchResults = props => (
+  <ul>
+    {props.articles.map((article, index) => (
+      <li key={index}>
+        <a href={article.webUrl} target="_blank">
+          {article.webTitle}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 export default SearchResults;
