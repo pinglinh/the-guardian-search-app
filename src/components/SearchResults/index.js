41de5e2a054c8657df58c02f2ecbe7bf2728326b
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-04-15 16:09:43 +0100

    Check for props before mapping

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 
 const SearchResults = props => (
   <ul>
-    {props.articles.map((article, index) => (
-      <li key={index}>
-        <a href={article.webUrl} target="_blank">
-          {article.webTitle}
-        </a>
-      </li>
-    ))}
+    {props.articles &&
+      props.articles.map((article, index) => (
+        <li key={index}>
+          <a href={article.webUrl} target="_blank">
+            {article.webTitle}
+          </a>
+        </li>
+      ))}
   </ul>
 );
 
