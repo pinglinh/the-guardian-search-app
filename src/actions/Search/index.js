e56ecc1511e79de326a70fe2f3890780fc4f7e93
pinglinh <lnguyenmy1@gmail.com>
2018-05-01 17:21:07 +0100

    Add Search action

--- src/actions/Search/index.js
+++ src/actions/Search/index.js
@@ -1,1 +1,16 @@
-export const 
+import { API_KEY } from "../../../config";
+
+export const GET_ARTICLES = "GET_ARTICLES";
+
+function performSearch(event) {
+  fetch(`http://content.guardianapis.com/search?q=${event}&api-key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data =>
+      dispatch({
+        type: GET_ARTICLES,
+        articles: data.response.results
+      })
+    );
+}
+
+export default performSearch;
