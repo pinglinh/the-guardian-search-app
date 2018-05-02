b3e118d993be970540b91d784f3744a8dabfc141
pinglinh <lnguyenmy1@gmail.com>
2018-05-02 17:22:02 +0100

    Add value state and rename API function

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,18 +1,27 @@
 import { connect } from "react-redux";
 
+import { SEARCH_INPUT_VALUE } from "../../actions/Search";
+
+import API from "../../actions/Search";
+
 import Search from "../../components/Search";
 
 const mapStateToProps = state => ({
-  articles: state.articles
+  articles: state.articles,
+  value: state.value
 });
 
 const mapDispatchToProps = dispatch => ({
-  performSearch: event => ({
+  handleChange: event => {
     dispatch({
-      type: GET_ARTICLES,
+      type: SEARCH_INPUT_VALUE,
       value: event.target.value
-    })
-  })
+    });
+  },
+  handleSubmit: (event, query) => {
+    event.preventDefault();
+    API.getArticles(dispatch, query);
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
