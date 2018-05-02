3b1b77d50630f525d8ddd94ace18295dc220ed57
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-05-03 00:11:27 +0100

    Extract action types

--- src/actions/Search/index.js
+++ src/actions/Search/index.js
@@ -3,18 +3,54 @@ import { API_KEY } from "../../../config";
 export const SEARCH_INPUT_VALUE = "SEARCH_INPUT_VALUE";
 export const SET_ARTICLES = "SET_ARTICLES";
 
-function getArticles(dispatch, query) {
-  fetch(`http://content.guardianapis.com/search?q=${query}&api-key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log("do we have data?", data);
-      dispatch({
-        type: SET_ARTICLES,
-        articles: data.response.results
-      });
-    });
+export function inputValue(text) {
+  return {
+    type: SEARCH_INPUT_VALUE,
+    text
+  };
 }
 
-export default {
-  getArticles
-};
+export function receivedArticles(articles) {
+  return {
+    type: SET_ARTICLES,
+    articles
+  };
+}
+
+function getArticles(query) {
+  return function(dispatch) {
+    return fetch(
+      `http://content.guardianapis.com/search?q=${query}&api-key=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log("do we have data?", data);
+        dispatch(receivedArticles(data.response.results));
+      });
+  };
+}
+
+// var counterFactory = function() {
+//   var i = 0;
+//   return function() {
+//     return i++;
+//   };
+// };
+//
+// var counter = counterFactory();
+//
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+//
+// const fetchPostsIfNeeded = function(subreddit) {
+//   return function(dispatch, getState) {
+//     if (shouldFetchPosts(getState(), subreddit)) {
+//       return dispatch(fetchPosts(subreddit));
+//     }
+//   };
+// };
+//
+// dispatch(fetchPostsIfNeeded(selectedSubreddit))
+
+export default getArticles;
