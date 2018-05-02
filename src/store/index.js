14aac25e07810bffef34451590044765427df243
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-05-03 00:15:14 +0100

    Pass thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { createStore } from "redux";
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 // import reducers from "../reducers";
 import SearchReducer from "../reducers/Search";
 
-const store = createStore(SearchReducer);
+const store = createStore(SearchReducer, applyMiddleware(thunk));
 
 export default store;
