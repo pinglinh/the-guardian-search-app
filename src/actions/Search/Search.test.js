5d00941ddc1239992dec931e882593dc9bb8e849
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-05-09 19:47:55 +0100

    Add test for inputValue and receivedArticles action creators

--- src/actions/Search/Search.test.js
+++ src/actions/Search/Search.test.js
@@ -1,5 +1,25 @@
 import * as SearchAction from "./";
 
 describe("Search action", () => {
-  it("");
+  it("it should create an action to get user's input value", () => {
+    const text = "Hello";
+
+    const expectedAction = {
+      type: SearchAction.SEARCH_INPUT_VALUE,
+      text
+    };
+
+    expect(SearchAction.inputValue(text)).toEqual(expectedAction);
+  });
+
+  it("it should create an action to set the articles", () => {
+    const articles = ["hello", "hello"];
+
+    const expectedAction = {
+      type: SearchAction.SET_ARTICLES,
+      articles
+    };
+
+    expect(SearchAction.receivedArticles(articles)).toEqual(expectedAction);
+  });
 });
