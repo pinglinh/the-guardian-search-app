29e5f1c80f4519dc36dd63429a841170e4585ca2
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-05-10 00:11:41 +0100

    Add test for receivedArticles and getArticles function

--- src/actions/Search/Search.test.js
+++ src/actions/Search/Search.test.js
@@ -1,4 +1,9 @@
 import * as SearchAction from "./";
+import { API_KEY } from "../../../config";
+
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
 
 describe("Search action", () => {
   it("it should create an action to get user's input value", () => {
@@ -21,5 +26,38 @@ describe("Search action", () => {
     };
 
     expect(SearchAction.receivedArticles(articles)).toEqual(expectedAction);
+  });
+
+  it("it should create SET_ARTICLES when fetching articles has been done", () => {
+    const middleware = [thunk];
+    const mockStore = configureMockStore(middleware);
+    const query = "hello";
+
+    fetchMock.getOnce(
+      `http://content.guardianapis.com/search?q=${query}&api-key=${API_KEY}`,
+      {
+        body: {
+          response: {
+            results: ["hello this should be a results of objects btw"]
+          }
+        },
+        headers: {
+          "content-type": "application/json"
+        }
+      }
+    );
+
+    const expectedAction = {
+      type: SearchAction.SET_ARTICLES,
+      articles: ["hello this should be a results of objects btw"]
+    };
+
+    const store = mockStore({});
+
+    return store.dispatch(SearchAction.getArticles(query)).then(() => {
+      console.log(store.getActions());
+      expect(store.getActions()[0]).toEqual(expectedAction);
+      // can also do getActions and make expectedAction into []
+    });
   });
 });
