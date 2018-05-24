71bb7bba43172d3475f7611ad4d84edc717c8d0d
Linh Nguyen My <lnguyenmy1@gmail.com>
2018-05-24 19:35:29 +0100

    Import Search reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 // import reducers from "../reducers";
-import SearchReducer from "../reducers/Search";
+import { SearchReducer } from "../reducers/Search";
 
 const store = createStore(SearchReducer, applyMiddleware(thunk));
 
